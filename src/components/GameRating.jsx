86a0bc6607bd2dce5a8c156cdab5c40c4fec246f
Shiddiq Mukhlas <mukhlashiddiq@gmail.com>
2026-05-28 18:08:22 +0700

    fix issue

--- src/components/GameRating.jsx
+++ src/components/GameRating.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import './gameRating.css'
-function GameRating({rating}) {
+
+function GameRating({ rating }) {
     const [stars, setStars] = useState([])
 
-    const generateStars=()=> {
+    const generateStars = () => {
         let stars = []
-        if(rating>5 || rating <1){
-            return
+
+        if (rating > 5 || rating < 1) {
+            return []
         }
-        for(let i = 0; i < rating; i++){
+
+        for (let i = 0; i < rating; i++) {
             stars.push(i)
         }
+
         return stars
     }
 
@@ -18,13 +22,13 @@ function GameRating({rating}) {
         setStars(generateStars())
     }, [rating])
 
-  return (
-    <div className='gameRating'>
-        {stars.map((star, index) => (
-            <i className="bi bi-star-fill" key={star}></i>
-        ))}
-    </div>
-  )
+    return (
+        <div className='gameRating'>
+            {stars.map((star) => (
+                <i className="bi bi-star-fill" key={star}></i>
+            ))}
+        </div>
+    )
 }
 
 export default GameRating
