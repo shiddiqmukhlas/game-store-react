b4429735ff929aac861b1e80365c03ec10709b54
Shiddiq Mukhlas <mukhlashiddiq@gmail.com>
2026-05-28 18:23:02 +0700

    fix issue

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -14,7 +14,7 @@ function Main() {
   }
 
   const fetchData = () => {
-    fetch('http://localhost:3000/api/gamesData.json')
+    fetch('/api/gamesData.json')
     .then(res => res.json())
     .then(data => {
       setGames(data)
